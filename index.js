aee57d57c1c1dba77cda1433419ca33cdba4e841
Robin <+>
2023-12-20 10:49:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+//multiple
+let multiply;
+let num1= 62;
+let num2= 1;
+
+multiply=num1*num2;
+
+//Create random
+let random = Math.floor(Math.random())+1;
+    console.log(random);
+
+//Create mod
+let mod;
+let num3= 204;
+let num4= 50;
+mod= num3%num4;
+
+//Create max
+let max = Math.max(1,2,4,8,16,20);
+console.log (max);
